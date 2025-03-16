e3dfe00eef4e3f8d53dd4884a56e1f37b3ec455a
tonetheman <tonetheman@gmail.com>
2025-03-16 15:35:16 -0500

    two images both can drag

--- image_loader.lua
+++ image_loader.lua
@@ -3,7 +3,8 @@
 local ImageLoader = Object:extend()
 function ImageLoader:new()
     self.imgs = {}
-    self.imgs["f"] = love.graphics.newImage("assets/runeGrey_rectangle_001_64x64.png")
+    self.imgs["gf"] = love.graphics.newImage("assets/runeGrey_rectangle_001_64x64.png")
+    self.imgs["bf"] = love.graphics.newImage("assets/runeBlue_rectangle_001_64x64.png")
 end
 
 
